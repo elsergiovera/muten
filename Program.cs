4fadc90e8c254c401de4eaeb3a959b4bb39c7238
elsergio <elsergio@gmail.com>
2024-04-13 21:45:38 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] PARAMS = ["-a", "-app", "-application"];
+string? PROCESS = null;
+
+if (args.Length != 2)
+    Console.Error.WriteLine("Invalid number of arguments");
+else {
+    if (PARAMS.Contains(args[0]))
+        PROCESS = args[1];
+
+    if (null != PROCESS)
+        Console.WriteLine(string.Format("parameter={0}", PROCESS));
+    else {
+        Console.Error.WriteLine("No valid arguments");
+        Environment.Exit(0);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# muten
+# MUTEn
+
+ :construction_worker::no_entry: Under construction :no_entry::construction_worker:
